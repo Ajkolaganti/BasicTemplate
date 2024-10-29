84b3a5e4dec26206fefd645b8fca868b1f402d97
Ajkolaganti <kolagantiaj1@gmail.com>
2024-10-28 22:46:00 -0400

    Initial commit

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBAC7CCwa8cWvjexMH6knI0ZfE5-tHw3Xs",
-    authDomain: "image-ai-6e4d8.firebaseapp.com",
-    projectId: "image-ai-6e4d8",
-    storageBucket: "image-ai-6e4d8.appspot.com",
-    messagingSenderId: "139558132758",
-    appId: "1:139558132758:web:941484e701af40ba50a937",
-    measurementId: "G-RHY2PCHHYH"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app); 
